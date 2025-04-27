b6d1ceefe844f3f256560ad0147f9326b1806ce3
s31723 <s31723@pjwstk.edu.pl>
2025-04-27 17:36:46 +0200

    list all devices perosnal computers still to do embedbed devices and smartwatch and udpate id every other methiod works for device

--- src/DeviceManager.WebApi/Program.cs
+++ src/DeviceManager.WebApi/Program.cs
@@ -1,4 +1,5 @@
 using Device.BusinessLogic.Services;
+using DeviceManager.Entries.Devices;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -30,5 +31,82 @@ app.MapGet("/api/devices", (IServiceMethods serviceMethod) =>
     })
     .WithName("GetAllDevices")
     .WithOpenApi();
+
+app.MapPost("/api/devices", (IServiceMethods serviceMethod, DeviceNonAbstractClass device) =>
+{
+    try
+    {
+        var result = serviceMethod.addDevice(device);
+        if (result is true)
+        {
+            return Results.Created();
+        }
+        else
+        {
+
+
+            return Results.BadRequest();
+
+        }
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem(ex.Message);
+    }
+});
+app.MapDelete("/api/devices/{id}", (IServiceMethods serviceMethod, string id) =>
+{
+    try
+    {
+        var result = serviceMethod.deleteDevice(id);
+        if (result is true)
+        {
+            return Results.Ok();
+        }
+        else
+        {
+            return Results.NotFound();
+        }
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem(ex.Message);
+    }
+});
+
+app.MapGet("/api/devices/{id}", (IServiceMethods serviceMethod, string id) =>
+{
+    try
+    {
+        var result = serviceMethod.getDeviceID(id);
+        if (result is not null)
+        {
+            return Results.Ok(result);
+        }
+        else
+        {
+            return Results.BadRequest();
+        }
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem(ex.Message);
+    }
+});
+
+app.MapGet("api/devices/personalComputers", (IServiceMethods serviceMethod) =>
+{
+    try
+    {
+        {
+            return Results.Ok(serviceMethod.getAllPersonalComputers());
+        }
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem(ex.Message);
+    }
+});
+
 
 app.Run();
--- src/DeviceManager.WebApi/obj/Debug/net8.0/DeviceManager.WebApi.AssemblyInfo.cs
+++ src/DeviceManager.WebApi/obj/Debug/net8.0/DeviceManager.WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DeviceManager.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf6337e9ef29e1eef4316626d58a5a3b54cf07a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c53fbce713b29446cb1840b5736ee440615db58")]
 [assembly: System.Reflection.AssemblyProductAttribute("DeviceManager.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DeviceManager.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
